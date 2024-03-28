22d537262315dba7c6d635b7df83bcf9df0c7a74
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-03-28 11:44:48 +0100

    counter ready

--- src/init.js
+++ src/init.js
@@ -1,1 +1,48 @@
-alert('init.js loaded!');
+// DOM
+const dom = {
+  counter: document.querySelector('.counter'),
+  decrement: document.getElementById('decrement'),
+  reset: document.getElementById('reset'),
+  increment: document.getElementById('increment'),
+  decrement2: document.getElementById('decrement2'),
+  increment2: document.getElementById('increment2'),
+};
+
+//Data
+const data = {
+  counter: 0,
+};
+
+//Handlers
+const decrementHandler = (par) => {
+  data.counter -= par;
+  dom.counter.innerText = data.counter;
+};
+
+const incrementHandler = (par) => {
+  data.counter += par;
+  dom.counter.innerText = data.counter;
+};
+const resetHandler = () => {
+  data.counter = 0;
+  dom.counter.innerText = 0;
+};
+
+// Event listeners
+dom.decrement.addEventListener('click', () => {
+  decrementHandler(1);
+});
+
+dom.decrement2.addEventListener('click', () => {
+  decrementHandler(2);
+});
+
+dom.reset.addEventListener('click', resetHandler);
+
+dom.increment.addEventListener('click', () => {
+  incrementHandler(1);
+});
+
+dom.increment2.addEventListener('click', () => {
+  incrementHandler(2);
+});
